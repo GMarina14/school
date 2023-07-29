f29627112e30c85f8781d3f917f8bf355230f769
GMarina14 <marika1429@gmail.com>
2023-07-29 13:48:42 +0300

    done

---
+++ src/main/java/ru/hogwarts/school/Repository/AvatarRepository.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.hogwarts.school.Model.Avatar;
+
+import java.util.Optional;
+
+@Repository
+public interface AvatarRepository extends JpaRepository<Avatar, Long> {
+    Optional<Avatar> findByStudentId(Long studentId);
+
+}
