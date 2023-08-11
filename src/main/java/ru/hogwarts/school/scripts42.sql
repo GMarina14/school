5ce33f9974abf6e71c4d1429127759b0579fae18
GMarina14 <marika1429@gmail.com>
2023-08-11 22:01:45 +0300

    Домашка выполнена

---
+++ src/main/java/ru/hogwarts/school/scripts42.sql
@@ -0,0 +1,5 @@
+alter table student add constraint age_constraint CHECK (age>=16);
+alter table student alter column name set not null;
+alter table student add constraint name UNIQUE(name);
+alter table faculty add constraint name_color  UNIQUE (name, color);
+alter table student alter column age set default 20;
